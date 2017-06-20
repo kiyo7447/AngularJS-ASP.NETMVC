6b70d9b366ba8b271b679dd34c9c9b0d130a4948
Kiyotaka Abe <k-abe@s-giken.co.jp>
2017-06-20 16:02:06 +0900

    プロジェクト ファイルを追加します。

---
+++ Web1/Scripts/controllers.js
@@ -0,0 +1,54 @@
+﻿'use strict';
+
+// Google Analytics Collection APIs Reference:
+// https://developers.google.com/analytics/devguides/collection/analyticsjs/
+
+var app = angular.module('app.controllers', []);
+app
+    // Path: /
+    .controller('HomeCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
+        $scope.$root.title = 'AngularJS SPA Template for Visual Studio';
+        $scope.$on('$viewContentLoaded', function () {
+            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+        });
+    }])
+
+    // Path: /about
+    .controller('AboutCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
+        $scope.$root.title = 'AngularJS SPA | About';
+        $scope.$on('$viewContentLoaded', function () {
+            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+        });
+    }])
+
+    // Path: /login
+    .controller('LoginCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
+        $scope.$root.title = 'AngularJS SPA | Sign In';
+        // TODO: Authorize a user
+        $scope.login = function () {
+            $location.path('/');
+            return false;
+        };
+        $scope.$on('$viewContentLoaded', function () {
+            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+        });
+    }])
+
+    // Path: /error/404
+    .controller('Error404Ctrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
+        $scope.$root.title = 'Error 404: Page Not Found';
+        $scope.$on('$viewContentLoaded', function () {
+            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+        });
+    }]);
+
+//app.config(function ($controllerProvider) {
+//	app.controller = function (name, controller) {
+//		$controllerProvider.register(name, controller);
+//	}
+//});
+
+//var con = app.controller['AboutController'];
+//console.log('con' + con);
+
+
