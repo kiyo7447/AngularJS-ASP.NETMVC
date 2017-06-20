06f0407f226baced8b7c5ef4fc66b7c624a6b420
Kiyotaka Abe <k-abe@s-giken.co.jp>
2017-06-21 08:42:38 +0900

    一旦完成

--- Web1/Scripts/controllers.js
+++ Web1/Scripts/controllers.js
@@ -3,23 +3,29 @@
 // Google Analytics Collection APIs Reference:
 // https://developers.google.com/analytics/devguides/collection/analyticsjs/
 
+console.log('load controller');
+
 var app = angular.module('app.controllers', []);
-app
-    // Path: /
+	app
+	// Path: /
     .controller('HomeCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
         $scope.$root.title = 'AngularJS SPA Template for Visual Studio';
+        $scope.test = 'test!';
         $scope.$on('$viewContentLoaded', function () {
+        	console.log('viewContentLoaded');
             $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
         });
     }])
 
     // Path: /about
-    .controller('AboutCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
-        $scope.$root.title = 'AngularJS SPA | About';
-        $scope.$on('$viewContentLoaded', function () {
-            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
-        });
-    }])
+    //.controller('AboutCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
+    //    $scope.$root.title = '2AngularJS SPA | About';
+    //    $scope.ok = 'hogehoge';
+    //    $scope.$on('$viewContentLoaded', function () {
+    //    	console.log('viewContentLoaded About');
+    //    	$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+    //    });
+    //}])
 
     // Path: /login
     .controller('LoginCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
@@ -42,13 +48,29 @@ app
         });
     }]);
 
-//app.config(function ($controllerProvider) {
-//	app.controller = function (name, controller) {
-//		$controllerProvider.register(name, controller);
-//	}
-//});
+	var app = angular.module('app.controllers');
 
-//var con = app.controller['AboutController'];
-//console.log('con' + con);
+	app.config(['$controllerProvider', function ($controllerProvider) {
+		app.$controllerProvider = $controllerProvider;
 
+		//app.con.register('AboutCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
+		//	$scope.$root.title = '22AngularJS SPA | About';
+		//	$scope.ok = '22hogehoge';
+		//	$scope.$on('$viewContentLoaded', function () {
+		//		console.log('22viewContentLoaded About');
+		//		$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+		//	})
+		//}]);
+	}]);
+
+
+//	var app = angular.module('app.controllers');
+//app.con.register('AboutCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
+//	$scope.$root.title = '22AngularJS SPA | About';
+//	$scope.ok = '22hogehoge';
+//	$scope.$on('$viewContentLoaded', function () {
+//		console.log('22viewContentLoaded About');
+//		$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+//	})
+//}]);
 
