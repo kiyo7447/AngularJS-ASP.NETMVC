27ab89af9aed584d64dc004fd91918f39f6ab921
Kiyotaka Abe <k-abe@s-giken.co.jp>
2017-06-21 10:02:15 +0900

    一旦クローズ

--- Web1/Scripts/controllers.js
+++ Web1/Scripts/controllers.js
@@ -5,16 +5,15 @@
 
 console.log('load app.controllers');
 
-var app = angular.module('app.controllers', []);
-	app
+angular.module('app.controllers', [])
 	// Path: /
     .controller('HomeCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
-        $scope.$root.title = 'Home Title';
-        $scope.message = 'Hello Home Page';
-        $scope.$on('$viewContentLoaded', function () {
-        	console.log('viewContentLoaded Home Controller');
-            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
-        });
+    	$scope.$root.title = 'Home Title';
+    	$scope.message = 'Hello Home Page';
+    	$scope.$on('$viewContentLoaded', function () {
+    		console.log('viewContentLoaded Home Controller');
+    		$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+    	});
     }])
 
     // Path: /about
@@ -29,40 +28,41 @@ var app = angular.module('app.controllers', []);
 
     // Path: /contacts
     .controller('ContactsCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
-        $scope.$root.title = 'Contacts Title';
-        $scope.message = 'Hello Contacts Page!';
-        $scope.$on('$viewContentLoaded', function () {
-        	console.log('viewContentLoaded Contacts');
-        	$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
-        });
+    	$scope.$root.title = 'Contacts Title';
+    	$scope.message = 'Hello Contacts Page!';
+    	$scope.$on('$viewContentLoaded', function () {
+    		console.log('viewContentLoaded Contacts');
+    		$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+    	});
     }])
 
     // Path: /login
     .controller('LoginCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
-        $scope.$root.title = 'AngularJS SPA | Sign In';
-        // TODO: Authorize a user
-        $scope.login = function () {
-            $location.path('/');
-            return false;
-        };
-        $scope.$on('$viewContentLoaded', function () {
-            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
-        });
+    	$scope.$root.title = 'AngularJS SPA | Sign In';
+    	// TODO: Authorize a user
+    	$scope.login = function () {
+    		$location.path('/');
+    		return false;
+    	};
+    	$scope.$on('$viewContentLoaded', function () {
+    		$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+    	});
     }])
 
     // Path: /error/404
     .controller('Error404Ctrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
-        $scope.$root.title = 'Error 404: Page Not Found';
-        $scope.$on('$viewContentLoaded', function () {
-            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
-        });
+    	$scope.$root.title = 'Error 404: Page Not Found';
+    	$scope.$on('$viewContentLoaded', function () {
+    		$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+    	});
     }]);
 
-	var app = angular.module('app.controllers');
+var app = angular.module('app.controllers');
 
-	app.config(['$controllerProvider', function ($controllerProvider) {
+app.config(['$controllerProvider', function ($controllerProvider) {
 		app.$controllerProvider = $controllerProvider;
 
+		//これは動いた
 		//app.con.register('AboutCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
 		//	$scope.$root.title = '22AngularJS SPA | About';
 		//	$scope.ok = '22hogehoge';
@@ -73,7 +73,7 @@ var app = angular.module('app.controllers', []);
 		//}]);
 	}]);
 
-
+//これも動いた
 //	var app = angular.module('app.controllers');
 //app.con.register('AboutCtrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
 //	$scope.$root.title = '22AngularJS SPA | About';
